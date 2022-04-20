adc2902ae8b24d5501aed61a93b246bbdbebadb6
spandey6395 <spandey6395@gmail.com>
2022-04-20 06:13:10 +0530

    assignmentMiddleware

--- src/index.js
+++ src/index.js
@@ -3,23 +3,29 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
-
+const moment = require('moment');  
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://spandey6395:R43s8If0R4EpfraA@cluster0.mknlo.mongodb.net/test", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
+
+
+let time = moment();
+app.use (function (req, res, next) {
+    let url= req.originalUrl
+    console.log(url)
+    let ip= req.ip
+    console.log(ip)
+    console.log(time.format('h:mm:ss a'));
+    console.log(time.format('YYYY,MM,DD'));
+    next()
+})
 
 app.use('/', route);
 
